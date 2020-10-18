cc89cc23450b0f8cb6147ba3e7215829d3bb3050
Priyankgupt <67186956+Priyankgupt@users.noreply.github.com>
2020-10-18 16:51:33 +0530

    8

---
+++ find all occ .cpp
@@ -0,0 +1,25 @@
+//find all occurrences
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+    char s[10],c;
+    int i;
+    printf("Enter  the string : ");
+    gets(s);
+    printf("Enter character to be searched: ");
+    c=getchar();
+    for(i=0;s[i];i++)
+    {
+    	if(s[i]==c)
+    	{
+		    printf("character '%c' found at location: %d\n ",c,i);
+
+		}
+ 	}
+
+
+
+
+    return 0;
+}
